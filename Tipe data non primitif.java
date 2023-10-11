fe2009b89531296ec6933d0ae19c8f1882e0d58f
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2023-10-11 17:57:20 +0800

    Hari ke 3
    Hari ke 3 membuat tipe data non primitif

---
+++ Tipe data non primitif.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main (String[] args) {
+        //tipe data non primitif.java
+        
+       Integer Berat = 48;
+        Byte Yu = -128;
+        Long Tinggi = 150L;
+        Character nilai = 'A';
+        Short Umur = 18;
+        Float Suhu = 234.5f;
+        Double Ukuran = 6.4;
+        Boolean Sp = true;
+        
+        
+        
+}}
