36f1235bd0f0f985acca3fef89d7128a654b199e
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2023-10-12 18:18:33 +0800

    Hari 4
    Inputan Nama,Kelas dan Nim

---
+++ Days4_inputan.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        
+        
+        System.out.println(" masukkan nama = " );
+        String nama = input.nextLine();
+        System.out.println(" masukkan kelas = " );
+        String kelas = input.nextLine();
+        System.out.println(" masukkan Nim = " );
+        String Nim = input.nextLine();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+}}
