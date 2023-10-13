4217e25d46444ae29389c64d050142684124c3db
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2023-10-13 15:28:22 +0800

    Hari 5

---
+++ Days5.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        System.out.print("masukkan Nopeserta=" );
+        String Nopeserta = input.nextLine();
+        System.out.print(" masukkan Nama="  );
+        String Nama = input.nextLine();
+        System.out.print(" masukkan Jeniskelamin=" );
+        String Jeniskelamin = input.nextLine();
+        System.out.print(" masukkan umur= " );
+        float umur = input.nextFloat();
+        System.out.print(" masukkan BB= " );
+        float BB = input.nextFloat();
+        System.out.print(" masukkan keaktifan=" );
+        boolean keaktifan = input.nextBoolean();
+        
+        System.out.println("Nopeserta" + Nopeserta);
+        System.out.println("Nama" + Nama);
+        System.out.println("Jeniskelamin" + Jeniskelamin);
+        System.out.println("umur" + umur);
+        System.out.println("BB" + BB);
+        System.out.println("keaktifan" + keaktifan);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+}}
