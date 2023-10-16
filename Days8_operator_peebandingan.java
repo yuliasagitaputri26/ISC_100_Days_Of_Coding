f7cc4e647ecc3b4e49349d94a083a3ce65ee768d
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2023-10-16 19:24:13 +0800

    Hari 8
    Membuat operator perbandingan

---
+++ Days8_operator_peebandingan.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+        
+        int y = 20;
+        int s = 10;
+        boolean hasil = y == s;
+        System.out.println("apakah y == s ?" + hasil);
+        
+        hasil = y != s;
+        System.out.println("apakah y != s ?" + hasil);
+        
+        hasil = y > s;
+        System.out.println("apakah y > s ? " + hasil);
+        
+        hasil = y < s;
+        System.out.println("apakah y < s ? " + hasil);
+        
+        hasil = y >= s;
+        System.out.println("apakah y >= s ? " + hasil);
+        
+        hasil = y <= s;
+        System.out.println("apakah y <= s ? " + hasil);
+        
+        
+        }
+            
+}
