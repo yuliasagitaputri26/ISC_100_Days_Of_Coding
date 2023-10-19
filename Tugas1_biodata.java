1c7766902d8a81b4f827a25501839b82fef3de7a
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2023-10-19 20:11:34 +0800

    Tugas 1

---
+++ Tugas1_biodata.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+        
+        String nama = "yulia sagita putri";
+        String TTL = "Topore";
+        String T = "06 juli 2005";
+        String Nim = "D0223022";
+        String Prodi = "Teknik Informatika";
+        String U = "Unsulbar";
+        int A = 2023;
+        
+        System.out.println("nama:" + nama);
+        System.out.println("TTL:" + TTL);
+        System.out.println("T:" + T);
+        System.out.println("Nim:" + Nim);
+        System.out.println("Prodi:" + Prodi);
+        System.out.println("U:" + U);
+        System.out.println("A:" + A);
+        
+    }
+
+        }
