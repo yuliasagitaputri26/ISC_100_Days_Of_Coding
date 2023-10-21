8f473c776ab3b9ff3a6583463ef4b7b200d531b1
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2023-10-22 07:05:09 +0800

    Hari 13

---
+++ Days13_operator_aritmatika.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+        Scanner input = new Scanner (System.in);
+        int y = 5, b = 6;
+        int j = y + b;
+        int s = y - b;
+        int g = y * b;
+        int k = y / b;
+        int h = y % b;
+        
+        System.out.println("hasil penjumlahan = " + j );
+        System.out.println("hasil pengurangan = " + s);
+        System.out.println("hasil perkalian = " + g);
+        System.out.println("hasil pembagian = " + k);
+        System.out.println("hasil sisa bagi = " + h);
+        
+        
+        
+}
+
+        
+    }
+
+        
+
+        
+        
+           
+        
+  
