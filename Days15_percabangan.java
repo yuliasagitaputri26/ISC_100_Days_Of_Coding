d8c61ae089be6a998d916749a9a46c90f7cb4935
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2023-10-23 20:22:52 +0800

    Hari 15

---
+++ Days15_percabangan.java
@@ -0,0 +1,20 @@
+package percabangan;
+public class Main {
+    public static void main (String[] args) {
+        
+        int a=3;
+        if (a==1) {
+            System.out.println(" nilai a = 1 ");
+        }
+        else if (a==2 ) {
+            System.out.println(" nilai a = 2 ");
+        }else if ( a==3 ) {
+            System.out.println(" nilai a = 3 ");
+        }else {
+            System.out.print(" nilai a tidak diketahui ");
+        }
+        
+}
+
+        
+    }
