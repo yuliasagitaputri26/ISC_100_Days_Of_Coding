401da361e3f1cf18fcda25463fd6ba9121c09bb1
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2023-10-24 16:02:58 +0800

    Hari 16

---
+++ Days16_Switch.java
@@ -0,0 +1,21 @@
+package Day16_Switch;
+public class Main {
+    public static void main (String[] args) {
+        
+        String pesan = "tiga";
+        switch(pesan) {
+            case "satu":
+                System.out.println("Nama");
+                break;
+            case "dua":
+                System.out.println("Saya");
+                break;
+            case "tiga":
+                System.out.println("Yulia sagita putri");
+                break;
+            
+        }
+    }
+}
+
+        
