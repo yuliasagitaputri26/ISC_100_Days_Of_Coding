026c35e705296d9f0971336e17b9fbedfa7d9210
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2023-10-31 17:49:00 +0800

    Hari 23

---
+++ Days23_while_loop.java
@@ -0,0 +1,12 @@
+package Days23;
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+       Scanner yu = new Scanner (System.in);
+        
+        int i = 4;
+         while (i <= 10){
+        System.out.println(" hai yulia " + i);
+        i++;
+    }
+    }}
