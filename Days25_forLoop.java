606d0162cd0425ba7846601b78b5d1631bbc65cb
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2023-11-02 16:11:35 +0800

    Hari 25

---
+++ Days25_forLoop.java
@@ -0,0 +1,12 @@
+package Days25;
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+       Scanner yu = new Scanner (System.in);
+        
+        System.out.println("masukkan angka");
+        for (int y = 6; y <= 40; y = y+4){
+            System.out.println(y);
+        }
+    }
+    }
