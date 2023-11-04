0983e8da54de35cdbe779681666e9a497a6eac2b
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2023-11-04 21:57:44 +0800

    Hari 27

---
+++ Days27_LoopBersarang.java
@@ -0,0 +1,16 @@
+package Days27;
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+       Scanner yu = new Scanner (System.in);
+        
+           System.out.println("membuat segitiga");
+        int y = 6;
+		       for( int i =5;  i>0; i--) {
+			       for (int g = 0; g < i; g++){
+	        			System.out.print("*");
+			      }
+		        	System.out.println("");
+	      	}
+	}}
+           
