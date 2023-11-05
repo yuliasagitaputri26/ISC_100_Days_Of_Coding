623860a28b14abaf9ef36c90a7c00414bbf3a936
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2023-11-05 20:12:09 +0800

    Hari 28
    Membuat persegi menggunakan Loop bersarang

---
+++ Days28_Loopbersarang.java
@@ -0,0 +1,16 @@
+package Days28;
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+       Scanner yu = new Scanner (System.in);
+        
+        System.out.println("membuat persegi");
+        int y = 6;
+         for (int i = 1; i < y; i++){
+            for (int g = 1; g < y; g++){
+                System.out.print(" * ");
+            }
+            System.out.println("");
+        }}}
+           
+           
