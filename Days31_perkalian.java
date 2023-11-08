c401a0fc996a5a25f2fed68acc9d155cb12a0b9e
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2023-11-08 18:56:33 +0800

    Hari 31
    Membuat sebuah inputan,dan dari hasil inputan tersebut menghasilkan sebuah perkalian

---
+++ Days31_perkalian.java
@@ -0,0 +1,16 @@
+package Days31;
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+       Scanner yu = new Scanner (System.in);
+        
+        System.out.print("masukkan angka = ");
+        int n = yu.nextInt();
+        
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i + " x " + n + " = " + n * i);
+        }    
+        }    
+        
+      	}
+        
