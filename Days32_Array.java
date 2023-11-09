cf14a3cc6c2f2498de3d170ec775f40afcd284e6
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2023-11-09 21:34:37 +0800

    Hari 32

---
+++ Days32_Array.java
@@ -0,0 +1,14 @@
+package Days32;
+
+public class Main {
+    public static void main (String[] args) {
+        
+      //Deklarasi Array
+        String [] saudara = {"sofyan","icha","arwin","sakina"};
+        
+        System.out.println(saudara[0]);
+        System.out.println(saudara[1]);
+        System.out.println(saudara[2]);
+        System.out.println(saudara[3]);
+        
+	}}
