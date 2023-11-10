83f570b0de4ee186910ae6d8c743b4f77e7e746a
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2023-11-10 20:06:49 +0800

    Hari 33

---
+++ Days33_Array2Dimensi.java
@@ -0,0 +1,24 @@
+package Days33;
+
+public class Main {
+    public static void main (String[] args) {
+        
+        //Array 2 Dimensi
+        
+        Character [][] huruf = {
+            {'A' , 'B' , 'C'},
+            {'D' , 'E' , 'F'},
+            {'G' , 'H' , 'I'},
+        };
+        
+        for(int i=0; i<3; i++){
+            for(int y=0; y<3; y++){
+                System.out.print(huruf[i][y]+" ");
+            }
+            System.out.println();
+            
+        }
+        
+        
+         
+	}}
