db888b51bdfcecadb7418d8ef3b2f24c1ee08fb7
yuliasagitaputri26 <147218632+yuliasagitaputri26@users.noreply.github.com>
2023-11-21 20:58:03 +0800

    Hari 44
    Membuat kalkulator salah

---
+++ Days44_KalkulatorSalah.java
@@ -0,0 +1,19 @@
+package Days44;
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args) {
+        
+        Scanner yu = new Scanner(System.in);
+        System.out.print("masukkan angka 1 : ");
+        int y =yu.nextInt();
+        System.out.print("masukkan angka 2 : ");
+        int c =yu.nextInt();
+        
+        System.out.println(y * c +4);
+        System.out.println(y / c +2);
+        
+  }}
+
+        
+        
+        
